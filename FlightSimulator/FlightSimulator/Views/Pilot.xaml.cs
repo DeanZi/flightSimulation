41556390d46421b6167d334daeb3c695ea7b1d6a
Simon Bitton <simon.bitton1@live.biu.ac.il>
2019-04-26 13:36:34 +0300

    Revert "revert"
    This reverts commit 7dff6ff8e3fd83869fc6d3de0dd06b1f09fac991.

---
+++ FlightSimulator/FlightSimulator/Views/Pilot.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FlightSimulator.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FlightSimulator.Views
+{
+    /// <summary>
+    /// Interaction logic for Pilot.xaml
+    /// </summary>
+    public partial class Pilot : UserControl
+    {
+        public Pilot()
+        {
+            PilotVM pilotVM;
+            InitializeComponent();
+            DataContext = pilotVM = new PilotVM();
+        }
+    }
+}
